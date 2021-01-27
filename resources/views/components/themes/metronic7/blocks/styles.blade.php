bc1b83c229f3253f0451de550f051e5cbdb08c4a
kamaltejag <kamal@pop-os.localdomain>
2021-01-28 00:18:15 +0530

    Initial Commit

---
+++ resources/views/components/themes/metronic7/blocks/styles.blade.php
@@ -0,0 +1,24 @@
+<!--begin::Fonts-->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
+<!--end::Fonts-->
+<!--begin::Page Vendors Styles(used by this page)-->
+<link href="{{ asset('themes/metronic/plugins/custom/fullcalendar/fullcalendar.bundle.css?v=7.0.5') }}" rel="stylesheet" type="text/css" />
+<!--end::Page Vendors Styles-->
+<!--begin::Global Theme Styles(used by all pages)-->
+<link href="{{ asset('themes/metronic/plugins/global/plugins.bundle.css?v=7.0.5') }} " rel="stylesheet" type="text/css" />
+<link href="{{ asset('themes/metronic/plugins/custom/prismjs/prismjs.bundle.css?v=7.0.5') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('themes/metronic/css/style.bundle.css?v=7.0.5') }}" rel="stylesheet" type="text/css" />
+<!--end::Global Theme Styles-->
+<!--begin::Layout Themes(used by all pages)-->
+<!--end::Layout Themes-->
+
+<!-- TinyMCE -->
+<script src="https://cdn.tiny.cloud/1/uf9ks88mbg3hxvgbpgvohq8gb9snzsutwzjeo2h95vcu9pef/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+
+<!-- Bootstrap -->
+<!-- <link href="{{ asset('themes/metronic/css/style.bundle.min.css') }} " rel="stylesheet" type="text/css" /> -->
+<!-----bootstrap links--------->
+<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous"> -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
